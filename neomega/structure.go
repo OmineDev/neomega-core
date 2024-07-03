fcbb4ebc3c13901230c5abecd51f60f8d3fde806
CMA2401PT <1634268014@qq.com>
2024-07-03 20:52:18 +0800

    fix mc structure decode

--- neomega/structure.go
+++ neomega/structure.go
@@ -42,7 +42,10 @@ func RangeSplits(start int, len int, algin int) (ranges [][2]int) {
 	ranges = make([][2]int, 0, (len+algin-1)/algin)
 	currentStart := start
 	for currentStart < start+len {
-		currentEnd := ((currentStart + algin) / algin) * algin
+		currentEnd := (currentStart / algin) * algin
+		if currentEnd <= currentStart {
+			currentEnd += algin
+		}
 		if currentEnd > start+len {
 			currentEnd = start + len
 		}
