eba8210f0976b8ff0fa7fde058994e8dc463cb3a
Happy2018new <chenbo79800@163.com>
2024-07-13 11:53:09 +0800

    minor changes on frame block actor NBT decode

--- minecraft/protocol/block_actors/general_actors/item_frame_block_actor.go
+++ minecraft/protocol/block_actors/general_actors/item_frame_block_actor.go
@@ -2,20 +2,47 @@ package general
 
 import (
 	"github.com/OmineDev/neomega-core/minecraft/protocol"
-	"github.com/OmineDev/neomega-core/minecraft/protocol/block_actors/fields"
 )
 
 // 描述 物品展示框 和 荧光物品展示框 的通用字段
 type ItemFrameBlockActor struct {
-	BlockActor       `mapstructure:",squash"`
-	fields.FrameItem `mapstructure:",omitempty,squash"`
+	BlockActor     `mapstructure:",squash"`
+	Item           *protocol.Item `mapstructure:"Item,omitempty"`           // TAG_Compound(10)
+	ItemDropChance *float32       `mapstructure:"ItemDropChance,omitempty"` // TAG_Float(6) = 1
+	ItemRotation   *float32       `mapstructure:"ItemRotation,omitempty"`   // TAG_Float(6) = 0
 }
 
 func (f *ItemFrameBlockActor) Marshal(r protocol.IO) {
-	fun := func() *fields.FrameItem {
-		return &f.FrameItem
+	var has byte
+
+	func1 := func() *protocol.Item {
+		if f.Item == nil {
+			f.Item = new(protocol.Item)
+		}
+		return f.Item
+	}
+	func2 := func() *float32 {
+		if f.ItemDropChance == nil {
+			f.ItemDropChance = new(float32)
+		}
+		return f.ItemDropChance
+	}
+	func3 := func() *float32 {
+		if f.ItemRotation == nil {
+			f.ItemRotation = new(float32)
+		}
+		return f.ItemRotation
+	}
+
+	if f.Item != nil && f.ItemDropChance != nil && f.ItemRotation != nil {
+		has = 1
 	}
 
 	protocol.Single(r, &f.BlockActor)
-	protocol.NBTOptionalMarshaler(r, &f.FrameItem, fun, true)
+	r.Uint8(&has)
+	if has == 1 {
+		protocol.NBTOptionalFunc(r, f.Item, func1, false, r.NBTItem)
+		protocol.NBTOptionalFunc(r, f.ItemRotation, func3, false, r.Float32)
+		protocol.NBTOptionalFunc(r, f.ItemDropChance, func2, false, r.Float32)
+	}
 }
