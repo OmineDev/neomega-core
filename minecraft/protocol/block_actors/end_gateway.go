fe7a82481cef67cf74db1918e5489c88ca37f288
Happy2018new <chenbo79800@163.com>
2024-07-14 10:37:28 +0800

    provide support for end gateway NBT decode

---
+++ minecraft/protocol/block_actors/end_gateway.go
@@ -0,0 +1,24 @@
+package block_actors
+
+import (
+	"github.com/OmineDev/neomega-core/minecraft/protocol"
+	general "github.com/OmineDev/neomega-core/minecraft/protocol/block_actors/general_actors"
+)
+
+// 末地折跃门方块
+type EndGateway struct {
+	general.BlockActor `mapstructure:",squash"`
+	Age                int32   `mapstructure:"Age"`                  // TAG_Int(4) = 0
+	ExitPortal         []int32 `mapstructure:"ExitPortal,omitempty"` // TAG_List[TAG_Int] (9[4])
+}
+
+// ID ...
+func (*EndGateway) ID() string {
+	return IDEndGateway
+}
+
+func (b *EndGateway) Marshal(io protocol.IO) {
+	protocol.Single(io, &b.BlockActor)
+	io.Varint32(&b.Age)
+	protocol.FuncSliceOfLen(io, 3, &b.ExitPortal, io.Varint32)
+}
