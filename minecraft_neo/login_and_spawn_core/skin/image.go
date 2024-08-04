4cb4ce6d2abd85d2236f41a3eb5765aba286e1c9
Happy2018new <chenbo79800@163.com>
2024-08-04 22:04:14 +0800

    optimize some comments (skin related)

--- minecraft_neo/login_and_spawn_core/skin/image.go
+++ minecraft_neo/login_and_spawn_core/skin/image.go
@@ -37,75 +37,90 @@ func makeDefaultSkin() *Skin {
 	}
 }
 
-/*
-从 url 指定的网址下载文件，
-并处理为有效的皮肤数据。
-*/
+// 从 url 指定的网址下载文件，
+// 并处理为有效的皮肤数据，
+// 然后保存在 skin 中
 func ProcessURLToSkin(url string) (skin *Skin, err error) {
-	// create default skin
+	// 初始化默认皮肤信息
+	var skinImageData []byte
 	skin = makeDefaultSkin()
-	// download skin file from remote server
+	// 从远程服务器下载皮肤文件
 	res, err := download_wrapper.DownloadMicroContent(url)
 	if err != nil {
 		return skin, fmt.Errorf("ProcessURLToSkin: %v", err)
 	}
-	// get skin data
-	skin.SkinImageData, skin.SkinGeometry = res, defaultSkinGeometry
-	if len(res) >= 4 && bytes.Equal(res[0:4], []byte("PK\x03\x04")) {
-		if err = convertZIPToSkin(skin, res); err != nil {
-			return skin, fmt.Errorf("ProcessURLToSkin: %v", err)
+	// 获取皮肤数据
+	{
+		// 如果这是一个普通的皮肤，
+		// 那么 res 就是该皮肤的 PNG 二进制形式，
+		// 并且该皮肤使用的骨架格式为默认格式
+		skin.FullSkinData, skin.SkinGeometry = res, defaultSkinGeometry
+		skinImageData = res
+		// 如果这是一个高级的皮肤(比如 4D 皮肤)，
+		// 那么 res 是一个压缩包，
+		// 我们需要处理这个压缩包以得到皮肤文件
+		if len(res) >= 4 && bytes.Equal(res[0:4], []byte("PK\x03\x04")) {
+			skinImageData, err = convertZIPToSkin(skin)
+			if err != nil {
+				return skin, fmt.Errorf("ProcessURLToSkin: %v", err)
+			}
 		}
 	}
-	// decode to image
-	img, err := convertToPNG(skin.SkinImageData)
+	// 将皮肤 PNG 二进制形式解码为图片
+	img, err := convertToPNG(skinImageData)
 	if err != nil {
 		return skin, fmt.Errorf("ProcessURLToSkin: %v", err)
 	}
-	// encode to pixels and return
+	// 设置皮肤像素、高度、宽度等数据
 	skin.SkinPixels = img.(*image.NRGBA).Pix
 	skin.SkinWidth, skin.SkinHight = img.Bounds().Dx(), img.Bounds().Dy()
+	// 返回值
 	return
 }
 
-// 从 zipData 指代的 ZIP 二进制数据负载提取皮肤数据。
-// skinImageData 代表皮肤的 PNG 二进制形式，
-// skinGeometry 代表皮肤的骨架信息。
-func convertZIPToSkin(skin *Skin, zipData []byte) (err error) {
-	// create reader
-	reader, err := zip.NewReader(bytes.NewReader(zipData), int64(len(zipData)))
+// 从 zipData 指代的 ZIP 二进制数据负载提取皮肤数据，
+// 并把处理好的皮肤数据保存在 skin 中，
+// 同时返回皮肤图片(PNG)的二进制表示
+func convertZIPToSkin(skin *Skin) (skinImageData []byte, err error) {
+	// 创建 ZIP 读取器
+	reader, err := zip.NewReader(
+		bytes.NewReader(skin.FullSkinData),
+		int64(len(skin.FullSkinData)),
+	)
 	if err != nil {
-		return fmt.Errorf("ConvertZIPToSkin: %v", err)
+		return nil, fmt.Errorf("convertZIPToSkin: %v", err)
 	}
-	// set default resource patch
+	// 设置皮肤默认资源路径
 	skin.SkinResourcePatch = defaultSkinResourcePatch
-	// find skin contents
+	// 查找皮肤内容
 	for _, file := range reader.File {
-		// skin data
+		// 皮肤数据
 		if strings.HasSuffix(file.Name, ".png") && !strings.HasSuffix(file.Name, "_bloom.png") {
 			r, err := file.Open()
 			if err != nil {
-				return fmt.Errorf("ConvertZIPToSkin: %v", err)
+				return nil, fmt.Errorf("convertZIPToSkin: %v", err)
 			}
 			defer r.Close()
-			skin.SkinImageData, err = io.ReadAll(r)
+			skinImageData, err = io.ReadAll(r)
 			if err != nil {
-				return fmt.Errorf("ConvertZIPToSkin: %v", err)
+				return nil, fmt.Errorf("convertZIPToSkin: %v", err)
 			}
 		}
-		// skin geometry
+		// 皮肤骨架信息
 		if strings.HasSuffix(file.Name, "geometry.json") {
 			r, err := file.Open()
 			if err != nil {
-				return fmt.Errorf("ConvertZIPToSkin: %v", err)
+				return nil, fmt.Errorf("convertZIPToSkin: %v", err)
 			}
 			defer r.Close()
 			geometryData, err := io.ReadAll(r)
 			if err != nil {
-				return fmt.Errorf("ConvertZIPToSkin: %v", err)
+				return nil, fmt.Errorf("convertZIPToSkin: %v", err)
 			}
 			processGeometry(skin, geometryData)
 		}
 	}
+	// 返回值
 	return
 }
 
@@ -114,7 +129,7 @@ func convertToPNG(imageData []byte) (image.Image, error) {
 	buffer := bytes.NewBuffer(imageData)
 	img, err := png.Decode(buffer)
 	if err != nil {
-		return nil, fmt.Errorf("ConvertToPNG: %v", err)
+		return nil, fmt.Errorf("convertToPNG: %v", err)
 	}
 	return img, nil
 }
--- minecraft_neo/login_and_spawn_core/skin/skin.go
+++ minecraft_neo/login_and_spawn_core/skin/skin.go
@@ -1,17 +1,23 @@
 package skin
 
-/*
-skinImageData 指代皮肤的 PNG 二进制形式，
-skinData 指代皮肤的一维的密集像素矩阵，
-skinGeometryData 指代皮肤的骨架信息，
-skinWidth 和 skinHight 则分别指代皮肤的
-宽度和高度。
-*/
+// 描述皮肤信息
 type Skin struct {
-	SkinImageData     []byte
-	SkinPixels        []byte
-	SkinGeometry      []byte
+	// 储存皮肤数据的二进制负载。
+	// 对于普通皮肤，这是一个二进制形式的 PNG；
+	// 对于高级皮肤(如 4D 皮肤)，
+	// 这是一个压缩包形式的二进制表示
+	FullSkinData []byte
+	// 皮肤的一维密集像素矩阵
+	SkinPixels []byte
+	// 皮肤的骨架信息
+	SkinGeometry []byte
+	// SkinResourcePatch 是一个 JSON 编码对象，
+	// 其中包含一些指向皮肤所具有的几何形状的字段。
+	// 它包含的 JSON 对象指定动画的几何形状，
+	// 以及播放器的默认皮肤的组合方式
 	SkinResourcePatch []byte
-	SkinWidth         int
-	SkinHight         int
+	// 皮肤的宽度
+	SkinWidth int
+	// 皮肤的高度
+	SkinHight int
 }
